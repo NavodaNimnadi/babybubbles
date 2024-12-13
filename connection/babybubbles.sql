b5da5049382a4c99da6ed6b28debd1e03d3971c8
NavodaNimnadi <139615090+NavodaNimnadi@users.noreply.github.com>
2024-12-13 02:03:26 +0000

    change babybubbles sql file

--- connection/babybubbles.sql
+++ connection/babybubbles.sql
@@ -1,3 +1,4 @@
 create database babybubbles;
 use babybubbles;
 create table users(id INT primary key auto_increment,fullname VARCHAR(255),mail VARCHAR(255) UNIQUE,pwsd VARCHAR(255));
+insert into users values(1,"Navoda Nimnadi","nimnadi@gmail.com","a123");
